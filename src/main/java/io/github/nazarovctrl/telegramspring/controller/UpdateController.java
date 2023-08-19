7ebb96c7ebd4a28106559aa62ec527d12e99331e
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2023-08-19 21:36:52 +0500

    Enter removed

--- src/main/java/io/github/nazarovctrl/telegramspring/controller/UpdateController.java
+++ src/main/java/io/github/nazarovctrl/telegramspring/controller/UpdateController.java
@@ -3,5 +3,4 @@ package io.github.nazarovctrl.telegramspring.controller;
 import io.github.nazarovctrl.telegramspring.function.Controller;
 
 public abstract class UpdateController implements Controller {
-
 }
