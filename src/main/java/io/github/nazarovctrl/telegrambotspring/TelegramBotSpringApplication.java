c84ca5c038b53a414495bb980dc079357f98db99
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2023-10-29 14:17:19 +0500

    Testing bug fixed

--- src/main/java/io/github/nazarovctrl/telegrambotspring/TelegramBotSpringApplication.java
+++ src/main/java/io/github/nazarovctrl/telegrambotspring/TelegramBotSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TelegramSpringApplication {
+public class TelegramBotSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TelegramSpringApplication.class, args);
+        SpringApplication.run(TelegramBotSpringApplication.class, args);
     }
 
 }
--- src/main/java/io/github/nazarovctrl/telegrambotspring/bot/BotConfig.java
+++ src/main/java/io/github/nazarovctrl/telegrambotspring/bot/BotConfig.java
@@ -1,7 +1,6 @@
 package io.github.nazarovctrl.telegrambotspring.bot;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Component;
  * @author Azimjon Nazarov
  */
 @Component
-@PropertySource("application.properties")
 public class BotConfig {
 
     /**
